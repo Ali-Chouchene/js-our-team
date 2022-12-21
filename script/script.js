986d8113ad8aabbc316d24b5b426ce508c30ff0b
Ali-Chouchene <choucheneali9801@gmail.com>
2022-12-21 15:36:19 +0100

    finish

--- script/script.js
+++ script/script.js
@@ -21,3 +21,70 @@ Consigli del giorno:
 Ragioniamo come sempre a step.
 Prima la logica in italiano e poi traduciamo in codice.
 */
+
+
+//!array
+const team = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        img: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        img: "angela-caroll-chief-editor.jpg",
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        img: "walter-gordon-office-manager.jpg",
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        img: "scott-estrada-developer.jpg",
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg",
+    },
+];
+
+
+//! prendo elementi dal dom
+const row = document.getElementById("row");
+
+//!preparo variabile vuota nella quale scrivero l'HTML
+let htmlCode = ``;
+
+//!ciclo for con il quale prendero manmano ogni oggetto
+for (let i = 0; i < team.length; i++) {
+    //!costante che mi da l'oggetto attuale
+    const currentI = team[i];
+
+    //!monto ogni elemento
+    htmlCode +=
+        `
+            <div class="col px-2 d-flex justify-content-center">
+                <div class="card rounded-0">
+                    <img src="${"img/img/" + currentI.img}">
+                    <div class="card-body">
+                      <h5  class="card-title">${currentI.name}</h5>
+                      <p id="position" class="card-text">${currentI.role}</p>
+                    </div>
+                </div>
+            </div>
+`;
+
+
+};
+
+//!stampo in pagina
+row.innerHTML = htmlCode;
